15cb23d0448273851008ff88b28fd1a6c9e39d14
AI Lab User7-1 <ailabuser7-1@AIdeMac-Studio.local>
2025-05-19 14:57:18 +0800

    rewrite finetune base on Qwen-2.5-coder

---
+++ finetuning/sft/scripts/sft_qwencoder_mac.sh
@@ -0,0 +1,49 @@
+export PATH=/path/to/miniconda3/envs/qwen-coder-sft/bin:$PATH;
+
+MODEL_SIZE=${1}
+DATA_PATH=${2}
+PRETRAINED_MODEL=${3}
+OUTPUT_DIR=${4}
+
+if [ -z "$MODEL_SIZE" ]; then   
+    echo "MODEL_SIZE is not set"
+    exit 1
+fi
+
+DATA_PATH=${DATA_PATH:-"./data/processed/sft.jsonl"}
+PRETRAINED_MODEL=${PRETRAINED_MODEL:-"/Users/ailabuser7-1/Documents/models/Qwen2.5-Coder-${MODEL_SIZE}-Instruct/"}
+OUTPUT_DIR=${OUTPUT_DIR:-"./checkpoints/sft_model/${MODEL_SIZE}"}
+
+LR=5e-5
+MIN_LR=5e-6
+WARMUP_STEPS=100
+WEIGHT_DECAY=0.0
+MAX_LENGTH=1280
+
+echo "DATA_PATH: $DATA_PATH"
+echo "PRETRAINED_MODEL: $PRETRAINED_MODEL"
+echo "OUTPUT_DIR: $OUTPUT_DIR"
+echo "LR: $LR"
+echo "MIN_LR: $MIN_LR"
+echo "WARMUP_STEPS: $WARMUP_STEPS"
+echo "WEIGHT_DECAY: $WEIGHT_DECAY"
+echo "MAX_LENGTH: $MAX_LENGTH"
+
+cd /Users/ailabuser7-1/Documents/cursor-workspace/Qwen2.5-Coder/finetuning/sft/
+
+python train.py \
+    --model_name_or_path ${PRETRAINED_MODEL} \
+    --data_path $DATA_PATH \
+    --model_max_length ${MAX_LENGTH} \
+    --output_dir ${OUTPUT_DIR} \
+    --num_train_epochs 3 \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 100 \
+    --learning_rate ${LR} \
+    --weight_decay ${WEIGHT_DECAY} \
+    --warmup_steps ${WARMUP_STEPS} \
+    --lr_scheduler_type "cosine" \
+    --logging_strategy "steps" \
+    --logging_steps 1 \
+    --truncate_source False
