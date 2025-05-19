4e62149ee9b803089e182d4bdc714d95ce1fe8fa
AI Lab User7-1 <ailabuser7-1@AIdeMac-Studio.local>
2025-05-19 15:57:35 +0800

    README.md

--- finetuning/sft/scripts/binarize_data.sh
+++ finetuning/sft/scripts/binarize_data.sh
@@ -1,8 +1,8 @@
-export PATH=/path/to/miniconda3/envs/qwen/bin:$PATH;
+export PATH=/path/to/miniconda3/envs/qwen-coder-sft/bin:$PATH;
 INPUT_PATH=${1}
 OUTPUT_PATH=${2}
 TOKENIZER_PATH=${3}
-INPUT_PATH=${INPUT_PATH:-"./raw/sft.jsonl"}
-OUTPUT_PATH=${OUTPUT_PATH:-"./processed/sft.jsonl"}
-TOKENIZER_PATH=${TOKENIZER_PATH:-"./pretrained_models/Qwen/Qwen2___5-Coder-1___5B/"}
+INPUT_PATH=${INPUT_PATH:-"./data/raw/sft.jsonl"}
+OUTPUT_PATH=${OUTPUT_PATH:-"./data/processed/sft.jsonl"}
+TOKENIZER_PATH=${TOKENIZER_PATH:-"/Users/ailabuser7-1/Documents/models/Qwen2.5-Coder-32B-Instruct/"}
 python binarize_data.py -input_path ${INPUT_PATH} -output_path ${OUTPUT_PATH} -workers 64 -tokenizer_path ${TOKENIZER_PATH}  -save_format .jsonl
